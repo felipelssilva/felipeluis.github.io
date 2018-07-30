69a666d7014801d2c9a665dcb6fe40e892aabf59
Felipe Luis <felipeluis.6@gmail.com>
2018-07-29 21:28:34 -0300

    removendo pontos do slide home

--- index.php
+++ index.php
@@ -104,10 +104,7 @@
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
             <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
         </ol>
-
-
         <!-- Wrapper for Slides -->
         <div class="carousel-inner">
             <div class="item active">
