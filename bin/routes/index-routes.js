67b7ab0319229c3a18bf2ba1105efbf204a3549e
Felipe Luis <felipe.silva@fh.com.br>
2020-07-27 19:41:13 -0300

    routes

--- bin/routes/index-routes.js
+++ bin/routes/index-routes.js
@@ -7,4 +7,32 @@ router.get('/', (req, res, next) => {
   res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
 });
 
+router.get('/home', (req, res, next) => {
+  res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
+});
+
+router.get('/about-me', (req, res, next) => {
+  res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
+});
+
+router.get('/social-medias', (req, res, next) => {
+  res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
+});
+
+router.get('/my-projects', (req, res, next) => {
+  res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
+});
+
+router.get('/blog', (req, res, next) => {
+  res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
+});
+
+router.get('/blog/*', (req, res, next) => {
+  res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
+});
+
+router.get('/contact', (req, res, next) => {
+  res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
+});
+
 module.exports = router;
