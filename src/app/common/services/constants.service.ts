f94b7d74ed254b7cfefd280d9ba8704d796932b7
Felipe Luis <felipe.silva@fh.com.br>
2020-07-26 22:08:04 -0300

    add section of certificates in about me page

--- src/app/common/services/constants.service.ts
+++ src/app/common/services/constants.service.ts
@@ -8,37 +8,35 @@ export class ConstantsService {
   readonly keywords: string = 'job, dev, developer, freelancer, freela, web, desenvolvimento, front-end, frontend, sap, hybris, c4hana, hana, c4, c4 hana, jQuery, JavaScript, Vanilla, ES6, ES5, React, Angular, Grunt, Gulp, SASS, Less, CSS3';
   readonly urlBase: string = window.location.pathname;
   readonly actualUrl: string = window.location.href;
-  readonly year : number = new Date().getFullYear();
-  readonly projects : any = {
-    "projects": [
-      {
-        "title": "In my old website I used some these frameworks, languages and preprocessors:",
-        "released": true,
-        "image": "my-old-website.jpg",
-        "description": "<ul><li>PHP</li><li>JavaScript</li><li>jQuery</li><li>LESS</li><li>Gulp</li><li>Bootstrap 4</li></ul>",
-        "url": "https://felipeluis-last-version.herokuapp.com/"
-      },
-      {
-        "title": "In Uberaba TI I use some these frameworks, languages:",
-        "released": true,
-        "image": "uberaba-ti.jpg",
-        "description": "<ul><li>PHP - Laravel 5.7</li><li>React</li><li>jQuery</li><li>Font Awesome</li><li>Bootstrap 4</li></ul>",
-        "url": "https://uberabati.com.br/"
-      },
-      {
-        "title": "The Rentatec website was fully developed by me, using:",
-        "released": true,
-        "image": "rebranding-rentatec.jpg",
-        "description": "<ul><li>PHP - Wordpress</li><li>JavaScript</li><li>jQuery</li><li>CSS3</li><li>Bootstrap 3</li></ul>",
-        "url": "https://www.rentatec.com.br/"
-      },
-      {
-        "title": "The Verbal website had some internal/layout bugs fixed, using:",
-        "released": false,
-        "image": "agencia-verbal-comunicacoes.jpg",
-        "description": "<ul><li>PHP</li><li>JavaScript</li><li>jQuery</li><li>CSS</li><li>Bootstrap 3</li><li>Fixed some layout bug's</li></ul>",
-        "url": "http://verbal.com.br/"
-      }
-    ] 
-  };
+  readonly year: number = new Date().getFullYear();
+  readonly projects: any = [
+    {
+      "title": "In my old website I used some these frameworks, languages and preprocessors:",
+      "released": true,
+      "image": "my-old-website.jpg",
+      "description": "<ul><li>PHP</li><li>JavaScript</li><li>jQuery</li><li>LESS</li><li>Gulp</li><li>Bootstrap 4</li></ul>",
+      "url": "https://felipeluis-last-version.herokuapp.com/"
+    },
+    {
+      "title": "In Uberaba TI I use some these frameworks, languages:",
+      "released": true,
+      "image": "uberaba-ti.jpg",
+      "description": "<ul><li>PHP - Laravel 5.7</li><li>React</li><li>jQuery</li><li>Font Awesome</li><li>Bootstrap 4</li></ul>",
+      "url": "https://uberabati.com.br/"
+    },
+    {
+      "title": "The Rentatec website was fully developed by me, using:",
+      "released": true,
+      "image": "rebranding-rentatec.jpg",
+      "description": "<ul><li>PHP - Wordpress</li><li>JavaScript</li><li>jQuery</li><li>CSS3</li><li>Bootstrap 3</li></ul>",
+      "url": "https://www.rentatec.com.br/"
+    },
+    {
+      "title": "The Verbal website had some internal/layout bugs fixed, using:",
+      "released": false,
+      "image": "agencia-verbal-comunicacoes.jpg",
+      "description": "<ul><li>PHP</li><li>JavaScript</li><li>jQuery</li><li>CSS</li><li>Bootstrap 3</li><li>Fixed some layout bug's</li></ul>",
+      "url": "http://verbal.com.br/"
+    }
+  ];
 }
