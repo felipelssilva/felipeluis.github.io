ab269275ed157cf619350737d7321d5665b5bc54
felipe <felipeluis.6@gmail.com>
2021-02-11 17:08:42 -0300

    fix paths routes

--- bin/routes/index-routes.js
+++ bin/routes/index-routes.js
@@ -4,37 +4,37 @@ const path = require('path')
 const nomeApp = process.env.npm_package_name
 
 router
-  .get('/',
-    (req, res, next) => {
-      res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
-    })
-  .get('/home',
-    (req, res, next) => {
-      res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
-    })
-  .get('/about-me',
-    (req, res, next) => {
-      res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
-    })
-  .get('/social-medias',
-    (req, res, next) => {
-      res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
-    })
-  .get('/my-projects',
-    (req, res, next) => {
-      res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
-    })
-  .get('/blog',
-    (req, res, next) => {
-      res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
-    })
-  .get('/blog/*',
-    (req, res, next) => {
-      res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
-    })
-  .get('/contact',
-    (req, res, next) => {
-      res.sendFile(path.resolve(`dist/${nomeApp}/index.html`))
-    });
+    .get('/',
+        (req, res, next) => {
+            res.sendFile(path.resolve(`dist/${nomeApp}/browser/index.html`))
+        })
+    .get('/home',
+        (req, res, next) => {
+            res.sendFile(path.resolve(`dist/${nomeApp}/browser/index.html`))
+        })
+    .get('/about-me',
+        (req, res, next) => {
+            res.sendFile(path.resolve(`dist/${nomeApp}/browser/index.html`))
+        })
+    .get('/social-medias',
+        (req, res, next) => {
+            res.sendFile(path.resolve(`dist/${nomeApp}/browser/index.html`))
+        })
+    .get('/my-projects',
+        (req, res, next) => {
+            res.sendFile(path.resolve(`dist/${nomeApp}/browser/index.html`))
+        })
+    .get('/blog',
+        (req, res, next) => {
+            res.sendFile(path.resolve(`dist/${nomeApp}/browser/index.html`))
+        })
+    .get('/blog/*',
+        (req, res, next) => {
+            res.sendFile(path.resolve(`dist/${nomeApp}/browser/index.html`))
+        })
+    .get('/contact',
+        (req, res, next) => {
+            res.sendFile(path.resolve(`dist/${nomeApp}/browser/index.html`))
+        });
 
 module.exports = router;
