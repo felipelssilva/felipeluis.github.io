17a3c34809b05b1b8014f31c09a783556631a0c5
Felipe Luis <felipeluis.6@gmail.com>
2022-04-15 21:33:25 -0300

    update server config

--- server.js
+++ server.js
@@ -1,15 +1,61 @@
-//Importa as dependências que acabamos de instalar
-const express = require("express");
-const path = require("path");
+const app = require('./app');
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const httpProxy = require('http-proxy');
 
-const app = express();
+// PORT // based on express-generator
+function normalizePort(val) {
+  const port = parseInt(val, 10);
 
-// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + "/dist/felipeluis"));
+  if (isNaN(port)) {
+    return val;
+  }
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/felipeluis/browser/index.html"));
-});
+  if (port >= 0) {
+    return port;
+  }
 
-// Inicia a aplicação pela porta configurada
-app.listen(process.env.PORT || 8080);
+  return false;
+}
+
+const port = normalizePort(process.env.PORT || 3000);
+app.set('port', port);
+
+// error handler
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+
+    default:
+      throw error;
+  }
+}
+
+// listener handler
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+// server
+// httpProxy.createServer({
+//   target:'http://localhost:3000'
+// }).listen(4200);
+const server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+console.log(`Website is alive on ${port}!`);
